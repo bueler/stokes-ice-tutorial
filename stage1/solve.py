b37e34c828b26847e4a5eb44eb3821020bfe089b
Ed Bueler <elbueler@alaska.edu>
2021-04-25 15:34:41 -0800

    Better linear solver, and consistent with later stages.

--- stage1/solve.py
+++ stage1/solve.py
@@ -15,18 +15,20 @@ def D(w):               # strain-rate tensor
 
 g = 9.81                # m s-2
 rho = 910.0             # kg m-3
-nu = 1.0e+8             # Pa s
+nu0 = 1.0e+12           # Pa s
 
 fbody = Constant((0.0, - rho * g))
-F = nu * inner(D(u), D(v)) * dx \
+F = nu0 * inner(D(u), D(v)) * dx \
     - p * div(v) * dx \
     - div(u) * q * dx \
     - inner(fbody, v) * dx
 bcs = [ DirichletBC(Z.sub(0), Constant((0.0, 0.0)), (42,)) ]
 
 par = {'snes_type': 'ksponly',
+       'mat_type': 'aij',
        'ksp_type': 'preonly',
-       'pc_type': 'svd'}
+       'pc_type': 'lu',
+       'pc_factor_shift_type': 'inblocks'}
 solve(F == 0, up, bcs=bcs, options_prefix='s', solver_parameters=par)
 
 u, p = up.split()
